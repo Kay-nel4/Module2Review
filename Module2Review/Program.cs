2631fecc8e6a0faf87f43e570ce180faa66a65f7
Kayla Knelle <kaynell3234@gmail.com>
2025-10-21 11:58:17 -0400

    Finally added the grades for each student to the list.

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.ComponentModel;
 using Module2Review;
 
 var students = new List<Student>();
@@ -7,33 +8,54 @@ var students = new List<Student>();
 
 
 
+var sammee = new Student();
+sammee.Name = "Sammee";
+sammee.Id = 1;
+sammee.AddGrade(99.4);
+sammee.AddGrade(99.4, 94, 97, 98.5);
+students.Add(sammee);
 
 
-var Sammee = new Student();
-Sammee.Name = "Sammee";
-Sammee.Id = 1;
-students.Add(Sammee);
-
-var Buzz = new Student()
+var buzz = new Student()
 {
     Name = "Buzz",
     Id = 2,
 };
-students.Add(Buzz);
+buzz.AddGrade(97.5);
+buzz.AddGrade(98.5, 95, 98, 96);
+students.Add(buzz);
 
-var Grady = new Student("Grady", 3);
-students.Add(Grady);
+
+var grady = new Student("Grady", 3);
+grady.AddGrade(92);
+grady.AddGrade(97, 92.5, 93, 95);
+students.Add(grady);
 
 
-var Roobee = new Student()
+var roobee = new Student()
 {
     Name = "Roobee",
     Id = 4,
 };
-students.Add(Roobee);
+roobee.AddGrade(95.5);
+roobee.AddGrade(95.5, 94, 97, 96);
+students.Add(roobee);
 
-var Beanie = new Student();
-Beanie.Name = "Beanie";
-Beanie.Id = 5;
-students.Add(Beanie);
+var beanie = new Student();
+beanie.Name = "Beanie";
+beanie.Id = 5;
+beanie.AddGrade(94);
+beanie.AddGrade(94, 93, 97.5, 92);
+students.Add(beanie);
+
+
+Console.WriteLine("Id  Name     Grades");
+foreach (var student in students)
+{
+    
+    Console.WriteLine($"{student.Id} {student.Name}  {string.Join(", ", student.Grades)}");
+}
+
+
+
 
--- Module2Review/Student.cs
+++ Module2Review/Student.cs
@@ -4,11 +4,15 @@ public class Student
 {
    public string Name { get; set; }
     public int Id { get; set; }
-    public List<double>Grades { get; set; }
+    public List<double> Grades { get; set; } = new List<double>();
+
+    
+
+    
 
     public void AddGrade(double grade)
     {
-        Grades.Add((grade));
+        Grades.Add(grade);
     }
 
     public void AddGrade(params double[] grades)
